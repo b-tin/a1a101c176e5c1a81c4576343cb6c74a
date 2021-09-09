0f71bc41843890c3474aee73cad964582de48420
B Tin <b.tintuk@gmail.com>
2021-09-09 20:50:30 +0700

    templates

--- pages/views.py
+++ pages/views.py
@@ -1,8 +1,9 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 # Function will return to client
 def home_page(request):
-    return HttpResponse('Hello World!')
+    return render(request, 'pages/home.html')
 
 def about_page(request):
     return HttpResponse('About page')
